db1836aa41ba36a14303574cf4b07ae0688f98f6
Avadheshkumarchauhan <avadheaha@gmail.com>
2025-06-16 01:43:56 +0530

    add Mebmership file in repo

---
+++ jenny/MembershipOprLec19.py
@@ -0,0 +1,16 @@
+""""
+Multiline comment
+
+*Membership oprater is check value parsent and not parsent and 
+return true or false.
+"""
+
+print('A' in "Avadhesh");
+name="Python";
+print('p'in name);
+print('P'in name);
+print('Z'not in name);
+l=[1,3,6,-9,10,0];
+print(2 in l);
+print(1 in l)
+print(20 not in l);
