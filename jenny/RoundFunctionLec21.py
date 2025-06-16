31a104963e9cd8eed5d4bdb83b38fbdc241f0e12
Avadheshkumarchauhan <avadheaha@gmail.com>
2025-06-16 14:35:55 +0530

    add round function file in repositary

---
+++ jenny/RoundFunctionLec21.py
@@ -0,0 +1,18 @@
+print(round(2345));
+print(round(2345 ,2));
+print(round(2345 ,0));
+print(round(2345.755 ,0));
+print(round(23495.86787 ,2));
+print(round(23495.5 ));
+print(round(23495.55787 ,2));
+print(round(663 ,-3));
+print(round(23495.55787 ,-1));
+print(round(2349 ,-1));
+print(round(-12 ,-2));
+print(round(-12 ,2));
+print(round(53 ,-2))
+print(round(-8/3 ,2))
+print(round(-8/3 ))
+print(round(878.5 ));
+print(round(878.54 ));
+
