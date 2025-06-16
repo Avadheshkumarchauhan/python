70f8900d1804e921ff21972db2cf80ecca7cb80d
Avadheshkumarchauhan <avadheaha@gmail.com>
2025-06-16 15:00:18 +0530

    add FString function file in repositary

---
+++ jenny/FStringFunctionLec22.py
@@ -0,0 +1,5 @@
+name=input("Enter name : ");
+age=int(input("Enter age : "));
+height=eval(input("Enter height : "));
+print(f"My name is {name}. I am {age} year old. My height {height}.");
+print(f"{name}'s father age {age*2} year old.");
