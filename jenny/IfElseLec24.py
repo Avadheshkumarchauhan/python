5c42c8a0d59c2cb05b452dcbbf36cec11a84e704
Avadheshkumarchauhan <avadheaha@gmail.com>
2025-06-18 13:06:24 +0530

    add ifelse file in repo

---
+++ jenny/IfElseLec24.py
@@ -0,0 +1,8 @@
+height =int(input("Enter a height in feet : "));
+if height >3:
+    print("This is not ph");
+    print("If block executed");
+else:
+    print("This is a ph");
+    print("Else block executed");
+print("End statement executed ");
